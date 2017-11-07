43da2deafbed952296a38914aef6bcfd06347b06
Willy <willyray@gmail.com>
2017-11-07 13:22:05 -0700

    adding a second button

--- ButtonPush.py
+++ ButtonPush.py
@@ -13,12 +13,16 @@ on = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(18)
-    if input_state == False:
-        print('Button Pressed')
+    input_state1 = GPIO.input(18)
+    input_state2 = GPIO.input(16)
+    if input_state1 == False:
+        print('Button 1 Pressed')
         b.set_group(1, 'on', not on)
         on = not on
         print('Office Lights on: %s' % on)
         time.sleep(0.2)
+    if input_state2 == False:
+        print('Button 2 Pressed')
