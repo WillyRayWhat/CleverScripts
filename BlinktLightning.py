f10a2c8f29b8330eefde765fa62793b28e58bc09
Willy <willyray@gmail.com>
2017-11-06 19:10:21 -0700

    reformatted, added comments, created a bunch of constants

--- BlinktLightning.py
+++ BlinktLightning.py
@@ -1,40 +1,92 @@
+"""
+File Name: BlinktLighting.py
+Author: Willy Ray
+Date Created: October 2017
+
+Requirements:
+Numpy
+The Blinkt library described here:
+https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-blinkt
+
+Description:
+This script drives a Pimoroni Blinkt led array to simulate a
+non-deterministic lightning strike effect.
+
+Lightning is modeled as follows:
+
+A lighting flash is a single flash of the entire array, with a
+duration, and an intensity.  The flash function is the only
+function that directly addresses the Blinkt device.
+
+A lighting strike is a sequence of multiple lightning flashes,
+with flash intensity, duration and sleep-between-flash times drawn
+from probability distributions.
+
+The main loop samples intra-lightning strike times from
+a exponential probability distribution.
+
+"""
+
 import blinkt
 import time
 import numpy as np
 
 from blinkt import set_pixel, set_brightness, show, clear
 
-set_brightness(1)
+# Color of flashes. These can be modified for non-white flashes.
+R=255
+G=255
+B=255
+
+# max and min brightness for an individual flash
+min_intensity=.25
+max_intensity = 1
+
+# The mean duration for an individual flash, in seconds
+mean_flash_duration=.15
+# the mean duration for the time between flashes, in seconds
+mean_sleep_duration=.15
+
+# min, max and mean flash_count in a strike (initial value drawn from
+# possion distribution)
+min_flash_count = 3
+max_flash_count = 8
+mean_flash_count=4
+
+# mean duration of time between lightning strikes, in seconds
+mean_intra_strike_time = 5
 
 
-
-def flash(duration):
+def flash(duration, intensity):
     clear()
+    set_brightness(intensity*intensity_factor)
     for pix in range(0,7):
-        set_pixel(pix, 255,255,255)
+        set_pixel(pix, R,G,B)
     show()
     time.sleep(duration)
     clear()
     show()
 
 
-    
-
-def multi_flash():
-    flash_count=min(8,max(3,np.random.poisson(5)))
+def strike(flash_count):
     for i in range(0,flash_count):
-        dur=np.random.exponential(.15)
-        sleep_dur = np.random.exponential(.15)
-        intensity = np.random.uniform(0,1)
-        set_brightness(intensity)
-        flash(dur)
+        dur=np.random.exponential(mean_flash_duration)
+        sleep_dur = np.random.exponential(mean_sleep_duration)
+        intensity = np.random.uniform(min_intensity, max_intensity)
+        flash(dur,intensity)
         time.sleep(sleep_dur)
+
 
 def main_loop():
     while True:
-        time.sleep(np.random.exponential(5))
-        multi_flash()
+        next_strike_in = np.random.exponential(mean_intra_strike_time)
+        time.sleep(next_strike_in)
+
+        flash_count = np.random.poisson(mean_flash_count)
+        flash_count = max(min_flash_count, flash_count)
+        flash_count = min(max_flash_count, flash_count)
+
+        strike(flash_count)
+
 
 main_loop()
-        
-    
