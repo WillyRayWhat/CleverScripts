e1c6b781c57bc633d4bc100a245768851d022785
Willy <willyray@gmail.com>
2017-11-07 12:02:52 -0700

    testing button push

--- ButtonPush.py
+++ ButtonPush.py
@@ -7,6 +7,6 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     input_state = GPIO.input(18)
-    if not input_state:
+    if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
