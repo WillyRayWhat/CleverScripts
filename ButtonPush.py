e94badc7df59f3ff7501fda1f18e52c8ce6fa479
Willy <willyray@gmail.com>
2017-11-07 12:40:29 -0700

    hacking

--- ButtonPush.py
+++ ButtonPush.py
@@ -9,6 +9,7 @@ b = Bridge('192.168.1.227')
 
 print(b.get_group(1))
 b.set_group(1, 'on', False)
+on = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -17,6 +18,7 @@ while True:
     input_state = GPIO.input(18)
     if input_state == False:
         print('Button Pressed')
-        b.set_group(1, 'on', not b.get_group((1,'on')))
-        print('Office Lights on: %s' % b.get_group(1,'on'))
+        b.set_group(1, 'on', not on)
+        on = not on
+        print('Office Lights on: %s' % on)
         time.sleep(0.2)
