d987c85b9eb21931208fe0a2a99b49781993c044
Willy <willyray@gmail.com>
2017-11-19 15:16:54 -0700

    connect to hue bridge

--- ArduinoSerialInterface.py
+++ ArduinoSerialInterface.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 
 import serial
+from phue import Bridge
 
+
+b = Bridge('192.168.1.227')
+b.connect()
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
+lights = b.get_light_objects('id')
+
+kitchen_lights = []
+
+for i in [7, 9, 10, 12, 8, 11]:
+    kitchen_lights.append(lights[i])
 
 # read response back from Arduino
 while True:
     input = ser.readline()
     if input:
         print(input)
+        values = input.split(',')
+        for item in kitchen_lights.__iter__():
+            bright = item.brightness
+            item.transitiontime = 0
+            item.brightness = int(values[1])
 
