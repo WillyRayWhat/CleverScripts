6353554ba00b56c31820c2d89ec4b9c808eaaa41
Willy <willyray@gmail.com>
2017-11-19 15:08:31 -0700

    working

--- ArduinoSerialInterface.py
+++ ArduinoSerialInterface.py
@@ -4,28 +4,9 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-
-
-# read from Arduino
-
-input = ser.read()
-
-print ("Read input " + input.decode("utf-8") + " from Arduino")
-
-
-
-# write something back
-
-ser.write(b'A')
-
-
-
 # read response back from Arduino
+while True:
+    input = ser.readline()
+    if input:
+        print(input)
 
-for i in range (0,3):
-
-    input = ser.read()
-
-    input_number = ord(input)
-
-    print ("Read input back: " + str(input_number))
