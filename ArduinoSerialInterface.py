a82dfbd9274c3b80d03d8449a095f233840efa9e
Willy <you@example.com>
2017-11-19 22:51:14 +0000

    working

--- ArduinoSerialInterface.py
+++ ArduinoSerialInterface.py
@@ -2,10 +2,10 @@
 
 import serial
 import phue as hue
-
+import time
 
 b = hue.Bridge('192.168.1.227')
-b.connect()
+#b.connect()
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 lights = b.get_light_objects('id')
@@ -15,15 +15,22 @@ kitchen_lights = []
 for i in [7, 9, 10, 12, 8, 11]:
     kitchen_lights.append(lights[i])
 
+def isInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+       
+
 # read response back from Arduino
 while True:
     input = ser.readline()
     if input:
-        print(input)
-        values = input.split(',')
-        if values[1].isdigit():
-            for item in kitchen_lights.__iter__():
-                bright = item.brightness
-                item.transitiontime = 0
-                item.brightness = int(values[1])
+        try:
+            brightness = input.split(',')[1]
+            if isInt(brightness):
+                print(int(brightness))
+                kitchen_lights[0].transitiontime = 0
+                kitchen_lights[0].brightness = int(brightness)
 
